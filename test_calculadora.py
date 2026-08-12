96e78829947b11682efc5be36def5721d3c1bd7c
Esse ano eu me mato kk <vcpiaia@minha.fag.edu.br>
2026-08-12 11:54:41 +0000

    teste push

--- test_calculadora.py
+++ test_calculadora.py
@@ -6,7 +6,7 @@ from calculadora import calcular_total
 def test_total_sem_desconto():
     itens = [(10.0, 2), (5.0, 1)]
 
-    assert calcular_total(itens) == 25.0
+    assert calcular_total(itens) == 26.0
 
 
 def test_total_com_dez_por_cento_de_desconto():
@@ -18,3 +18,4 @@ def test_total_com_dez_por_cento_de_desconto():
 def test_desconto_invalido():
     with pytest.raises(ValueError):
         calcular_total([(100.0, 1)], desconto_percentual=110)
+
